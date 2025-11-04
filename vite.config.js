6dae115daff3d24e6f80b04328581931949f1a24
koscakera <90839847+koscakera@users.noreply.github.com>
2025-11-04 13:53:42 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'import react from '@vitejs/plugin-react-swc'-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/home/ubuntu/saocarlino-bordero/src",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": "/home/ubuntu/saocarlino-bordero/src",
+    },
+  },
   server: {
     port: 8080,
     allowedHosts: [
