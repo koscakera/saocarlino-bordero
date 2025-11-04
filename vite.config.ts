b885317a0a072591f8006d38f4d228d64cd7b883
koscakera <90839847+koscakera@users.noreply.github.com>
2025-11-04 11:21:28 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    8080-ip8q1qx3sgbioi0l71tj4-096a483e.manus.computer,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
